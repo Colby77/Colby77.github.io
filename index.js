1d88935931b53d0bff923c11bfea5c957d57b8f4
Colby Workman <cgw2319@gmail.com>
2021-12-02 13:20:57 -0700

    added tab buttons

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const projectPic = document.getElementById('project-pic')
 const swapPic = document.getElementById('swap-pic')
+const aboutCats = document.getElementsByClassName('about-cat')
+const tablinks = document.getElementsByClassName('tablink')
 
 let currentImg = 0
 
@@ -10,6 +12,21 @@ pictureSources = [
     'dndcreator4.png',
     'dndcreator5.png'
 ]
+
+const changeContent = (evt, contentName) => {
+    // var aboutCats, tablinks;
+    console.log(aboutCats)
+    // aboutCats = document.getElementsByClassName('about-cat')
+    for(let i = 0; i < aboutCats.length; i++){
+        aboutCats[i].style.display = "none"
+    }
+    // tablinks = document.getElementsByClassName('tablink')
+    for(let i = 0; i < tablinks.length; i++){
+        tablinks[i].className = tablinks[i].className.replace(' active', '')
+    }
+    document.getElementById(contentName).style.display = 'flex'
+    evt.currentTarget.className += ' active';
+}
 
 swapPic.addEventListener('click', () => {
     console.log(currentImg)
